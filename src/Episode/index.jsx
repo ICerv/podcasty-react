a516fd8b09bf02f305d831881205e8770db09aac
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-15 21:48:19 +0200

    Task completed

---
+++ src/Episode/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css'
+
+
+const Episode = ({ num, title, guest }) => (
+  <div className="episode">
+    <div className="episode__num">{num}</div>
+    <div className="episode__body">
+      <div className="episode__title">{title}</div>
+      <div className="episode__guest">{guest}</div>
+    </div>
+  </div>
+)
+
+export default Episode
